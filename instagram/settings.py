c90171d2841f210199f570f0634975ac9554c74e
JuanCarlos <melozajuancarlos@gmail.com>
2026-05-19 16:36:44 +0200

    feat: initialize posts, profiles, and notifications apps and register them in settings

--- instagram/settings.py
+++ instagram/settings.py
@@ -37,6 +37,12 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    'django_extensions',
+
+    'profiles',
+    'posts',
+    'notifications',
 ]
 
 MIDDLEWARE = [
