96b5e5914267cfc138c67b14cb1d521e94df99ec
JCMeloza <jcarlos@SysMijas.localdomain>
2026-05-22 13:57:14 +0200

    chore: downgrade Django to 5.2.14 and add template directory to settings; implement views for home, login, register, legal, and contact pages

--- instagram/urls.py
+++ instagram/urls.py
@@ -16,7 +16,14 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from .views import HomeView, LoginView, RegisterView, LegalView, ContactView
 
 urlpatterns = [
+    path('', HomeView.as_view(), name='home'),
+    path('login/', LoginView.as_view(), name='login'),
+    path('register/', RegisterView.as_view(), name='register'),
+    path('legal/', LegalView.as_view(), name='legal'),
+    path('contact/', ContactView.as_view(), name='contact'),
     path('admin/', admin.site.urls),
+
 ]
