c2f026a9752104b07fc35720bae8c34721e06798
JCMeloza <jcarlos@SysMijas.localdomain>
2026-05-23 12:02:49 +0200

    feat: add user registration form and view with success message; update login and register templates for better user experience

--- instagram/views.py
+++ instagram/views.py
@@ -1,5 +1,11 @@
+from django.contrib.auth.models import User
 from django.shortcuts import render
 from  django.views.generic import TemplateView
+from django.views.generic.edit import CreateView
+from django.urls import reverse_lazy
+from .forms import RegistrationForm
+from  django.contrib import messages
+
 
 # Create your views here.
 class HomeView(TemplateView):
@@ -8,8 +14,18 @@ class HomeView(TemplateView):
 class LoginView(TemplateView):
     template_name = "general/login.html"
 
-class RegisterView(TemplateView):
+class RegisterView(CreateView):
     template_name = "general/register.html"
+    model = User
+    success_url = reverse_lazy('login')
+    form_class = RegistrationForm
+
+    def form_valid(self,form):
+        messages.add_message(self.request, messages.SUCCESS, "Usuarios creado con exito")
+        return super(RegisterView,self).form_valid(form)
+
+
+
 
 class LegalView(TemplateView):
     template_name = "general/legal.html"
