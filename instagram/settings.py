b9ef4344e27e7bf644c432c7d02823ffa6cd68b3
JCMeloza <jcarlos@SysMijas.localdomain>
2026-05-23 12:02:14 +0200

    chore: add crispy-forms and crispy-bootstrap5 to INSTALLED_APPS and configure template packs in settings

--- instagram/settings.py
+++ instagram/settings.py
@@ -40,6 +40,8 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
 
     'django_extensions',
+    'crispy_forms',
+    'crispy_bootstrap5',
 
     'profiles',
     'posts',
@@ -124,3 +126,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = 'static/'
+
+
+CRISPY_ALLOWED_TEMPLATE_PACKS = "bootstrap5"
+CRISPY_TEMPLATE_PACK = "bootstrap5"
